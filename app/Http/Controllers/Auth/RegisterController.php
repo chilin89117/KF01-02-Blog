1a789ebf8c8958605d31355254cfd425767505be
Chi Lin <chilin89117@gmail.com>
2018-01-28 17:25:21 -0800

    Override register() and registered() to add default profile and redirect user to login page

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,15 +1,20 @@
 <?php
 namespace App\Http\Controllers\Auth;
 use App\User;
+use App\Profile;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Storage;
 
 class RegisterController extends Controller
 {
   use RegistersUsers;
 
-  protected $redirectTo = '/admin';
+  protected $redirectTo = '/login';
 
   public function __construct()
   { $this->middleware('guest'); }
@@ -30,5 +35,26 @@ class RegisterController extends Controller
       'email' => $data['email'],
       'password' => bcrypt($data['password']),
     ]);
+  }
+
+  // Function overrides Illuminate\Foundation\Auth\RegistersUsers
+  public function register(Request $request)
+    {
+      $this->validator($request->all())->validate();
+      event(new Registered($user = $this->create($request->all())));
+      // $this->guard()->login($user);
+      return $this->registered($request, $user)
+                      ?: redirect($this->redirectPath());
+    }
+
+  // Function overrides Illuminate\Foundation\Auth\RegistersUsers
+  protected function registered(Request $request, $user)
+  {
+    $profile = Profile::create([
+      'user_id' => $user->id,
+      'avatar'  => "public/uploads/avatars/$user->id.jpg",
+    ]);
+    $file = new \SplFileInfo(base_path('default.jpg'));
+    Storage::putFileAs('public/uploads/avatars', $file, "$user->id.jpg");
   }
 }
